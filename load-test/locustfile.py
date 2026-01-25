cd468f4f9710fc44a099c03dfaa4226abe322b5a
KEKACHY <kekachy@mail.ru>
2026-01-25 18:24:59 +0200

    Initial commit

--- load-test/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class GlossaryUser(HttpUser):
-    wait_time = between(0.01, 0.05)
-
-    @task
-    def get_glossaries(self):
-        self.client.get("/glossaries/")
