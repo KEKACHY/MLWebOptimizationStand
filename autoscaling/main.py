5a3d953f56ec9989fb11f65b06d9b89e4535a2e5
KEKACHY <kekachy@mail.ru>
2026-01-28 02:04:22 +0200

    Autoscaling

--- autoscaling/main.py
+++ autoscaling/main.py
@@ -12,7 +12,7 @@ threshold = 18
 while True:
     try:
         # Запросим предсказание нагрузки с ml-service
-        response = requests.get('http://localhost:9000/current_prediction')
+        response = requests.get('http://ml-service:9000/current_prediction')
         predicted_load = response.json().get('predicted_load')
 
         if predicted_load and predicted_load[0] >= threshold:
